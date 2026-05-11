a7e4c1d5303f58b9e3db2b85596156bc44dc9ebf
Rion0-0 <rino.iwsk@gmail.com>
2026-05-11 17:25:14 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,12 +8,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "ここに入れる",
-  authDomain: "ここに入れる",
-  projectId: "ここに入れる",
-  storageBucket: "ここに入れる",
-  messagingSenderId: "ここに入れる",
-  appId: "ここに入れる"
+  apiKey: "AIzaSyA8eQWqxpxfMG8LEQgssRzcMJbn93JA5zg",
+  authDomain: "cycle-counter.firebaseapp.com",
+  projectId: "cycle-counter",
+  storageBucket: "cycle-counter.firebasestorage.app",
+  messagingSenderId: "466749972992",
+  appId: "1:466749972992:web:80de09778ad7926c28aee5",
+  measurementId: "G-PXRL8W1JNZ"
 };
 
 const app = initializeApp(firebaseConfig);
